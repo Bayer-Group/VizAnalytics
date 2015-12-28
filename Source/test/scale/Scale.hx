be752e21af4566ee1c50b686fabe0f4fcd6f8a7d
tccoll <thomas.cervantes.collier@monsanto.com>
2015-12-28 13:18:50 -0600

    fixed linear scale tests

--- Source/test/scale/Scale.hx
+++ Source/test/scale/Scale.hx
@@ -14,29 +14,30 @@ class Scale extends haxe.unit.TestCase {
 
   public function testDefualtLinear() {
     var scale = scale.Scale.linear();
+
     assertEquals(Std.string(scale.getDomain()),Std.string(defaultDomain));
     assertEquals(Std.string(scale.getRange()),Std.string(defaultRange));
-//    test clamp
-    assertEquals(Std.string(1),Std.string(scale.value(0)));
+
+    assertEquals(Std.string(0),Std.string(scale.value(0)));
 
     assertEquals(Std.string(1),Std.string(scale.value(1)));
     assertEquals(Std.string(100),Std.string(scale.value(100)));
-//    test clamp
-    assertEquals(Std.string(100),Std.string(scale.value(101)));
+
+    assertEquals(Std.string(101),Std.string(scale.value(101)));
 
     assertEquals(Std.string(50),Std.string(scale.value(50)));
 
   }
-  public function testLinearNoClamp(){
-    var scale = scale.Scale.linear().clamp(false);
+  public function testLinearClamp(){
+    var scale = scale.Scale.linear().clamp(true);
 
-    assertEquals(Std.string(0),Std.string(scale.value(0)));
+    assertEquals(Std.string(1),Std.string(scale.value(0)));
 
-    assertEquals(Std.string(-100),Std.string(scale.value(-100)));
+    assertEquals(Std.string(1),Std.string(scale.value(-100)));
 
-    assertEquals(Std.string(101),Std.string(scale.value(101)));
+    assertEquals(Std.string(100),Std.string(scale.value(101)));
 
-    assertEquals(Std.string(200),Std.string(scale.value(200)));
+    assertEquals(Std.string(100),Std.string(scale.value(200)));
   }
 
   public function testLinearDiffRangeAndDomainNoClamp(){
@@ -48,6 +49,7 @@ class Scale extends haxe.unit.TestCase {
 
     assertEquals(Std.string(50),Std.string(scale.value(100)));
 
-    assertEquals(Std.string(100),Std.string(scale.value(200)));
+    assertEquals(Std.string(75),Std.string(scale.value(200)));
   }
+
 }
