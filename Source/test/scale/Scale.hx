e067358adf91ba6a4dd633f27adf0a2e5b354893
tccoll <thomas.cervantes.collier@monsanto.com>
2015-12-28 16:35:42 -0600

    added linear invert test

--- Source/test/scale/Scale.hx
+++ Source/test/scale/Scale.hx
@@ -19,19 +19,26 @@ class Scale extends haxe.unit.TestCase {
     assertEquals(Std.string(scale.getRange()),Std.string(defaultRange));
 
     assertEquals(Std.string(0),Std.string(scale.value(0)));
+    assertEquals(Std.string(0),Std.string(scale.invert(0)));
 
     assertEquals(Std.string(1),Std.string(scale.value(1)));
+    assertEquals(Std.string(1),Std.string(scale.invert(1)));
+
     assertEquals(Std.string(100),Std.string(scale.value(100)));
+    assertEquals(Std.string(100),Std.string(scale.invert(100)));
 
     assertEquals(Std.string(101),Std.string(scale.value(101)));
+    assertEquals(Std.string(101),Std.string(scale.invert(101)));
 
     assertEquals(Std.string(50),Std.string(scale.value(50)));
+    assertEquals(Std.string(50),Std.string(scale.invert(50)));
 
   }
   public function testLinearClamp(){
     var scale = scale.Scale.linear().clamp(true);
 
     assertEquals(Std.string(1),Std.string(scale.value(0)));
+    assertEquals(Std.string(1),Std.string(scale.invert(1)));
 
     assertEquals(Std.string(1),Std.string(scale.value(-100)));
 
@@ -41,15 +48,18 @@ class Scale extends haxe.unit.TestCase {
   }
 
   public function testLinearDiffRangeAndDomainNoClamp(){
-    var scale = scale.Scale.linear().domain([-100,100]).range([0,50]).clamp(false);
+    var scale = scale.Scale.linear().range([1,50]);
 
-    assertEquals(Std.string(25),Std.string(scale.value(0)));
+    assertEquals(Std.string(0.505050505050505),Std.string(scale.value(0)));
+    assertEquals(Std.string(100),Std.string(scale.invert(50)));
 
-    assertEquals(Std.string(0),Std.string(scale.value(-100)));
+    assertEquals(Std.string(-48.989898989899),Std.string(scale.value(-100)));
+    assertEquals(Std.string(-99.9999999999818),Std.string(scale.invert(-48.98989898989)));
 
     assertEquals(Std.string(50),Std.string(scale.value(100)));
 
-    assertEquals(Std.string(75),Std.string(scale.value(200)));
+    assertEquals(Std.string(99.49494949494951),Std.string(scale.value(200)));
+    assertEquals(Std.string(200),Std.string(scale.invert(99.49494949494951)));
   }
 
 }
