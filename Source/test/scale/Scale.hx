2a433b3dee74016d1a0573708580b21ea0be85a4
tccoll <thomas.cervantes.collier@monsanto.com>
2015-12-29 11:54:14 -0600

    got rid of some dumb tests

--- Source/test/scale/Scale.hx
+++ Source/test/scale/Scale.hx
@@ -15,60 +15,65 @@ class Scale extends haxe.unit.TestCase {
   public function testDefualtLinear() {
     var scale = scale.Scale.linear();
 
-    assertEquals(Std.string(scale.getDomain()),Std.string(defaultDomain));
-    assertEquals(Std.string(scale.getRange()),Std.string(defaultRange));
+//    assertEquals(scale.getDomain(),defaultDomain);
+//    assertEquals(scale.getRange(),defaultRange);
 
-    assertEquals(Std.string(0),Std.string(scale.value(0)));
-    assertEquals(Std.string(0),Std.string(scale.invert(0)));
+    assertEquals(0,scale.value(0));
+    assertEquals(0,scale.invert(0));
 
-    assertEquals(Std.string(1),Std.string(scale.value(1)));
-    assertEquals(Std.string(1),Std.string(scale.invert(1)));
+    assertEquals(1,scale.value(1));
+    assertEquals(1,scale.invert(1));
 
-    assertEquals(Std.string(100),Std.string(scale.value(100)));
-    assertEquals(Std.string(100),Std.string(scale.invert(100)));
+    assertEquals(100,scale.value(100));
+    assertEquals(100,scale.invert(100));
 
-    assertEquals(Std.string(101),Std.string(scale.value(101)));
-    assertEquals(Std.string(101),Std.string(scale.invert(101)));
+    assertEquals(101,scale.value(101));
+    assertEquals(101,scale.invert(101));
 
-    assertEquals(Std.string(50),Std.string(scale.value(50)));
-    assertEquals(Std.string(50),Std.string(scale.invert(50)));
+    assertEquals(50,scale.value(50));
+    assertEquals(50,scale.invert(50));
 
   }
   public function testLinearClamp(){
     var scale = scale.Scale.linear().clamp(true);
 
-    assertEquals(Std.string(0),Std.string(scale.value(-1)));
+    assertEquals(0,scale.value(-1));
 
-    assertEquals(Std.string(1),Std.string(scale.invert(1)));
+    assertEquals(1,scale.invert(1));
 
-    assertEquals(Std.string(1),Std.string(scale.value(101)));
+    assertEquals(1,scale.value(101));
   }
 
   public function testLinearDiffRangeAndDomainNoClamp(){
     var scale = scale.Scale.linear().domain([1,100]).range([1,50]);
 
-    assertEquals(Std.string(0.505050505050505),Std.string(scale.value(0)));
+    assertEquals(0.505050505050505,scale.value(0));
 #if js
-    assertEquals(Std.string(99.99999999999999),Std.string(scale.invert(50)));
+    assertEquals(99.99999999999999,scale.invert(50));
 #else
+//    expected '100' but was '100' !!!!
     assertEquals(Std.string(100),Std.string(scale.invert(50)));
+
 #end
 
 
-    assertEquals(Std.string(-48.989898989899),Std.string(scale.value(-100)));
+    assertEquals(-48.989898989899,scale.value(-100));
 #if js
-    assertEquals(Std.string(-99.99999999998184),Std.string(scale.invert(-48.98989898989)));
+    assertEquals(-99.99999999998184,scale.invert(-48.98989898989));
 #else
+//    expected '-99.9999999999818' but was '-99.9999999999818'
     assertEquals(Std.string(-99.9999999999818),Std.string(scale.invert(-48.98989898989)));
 #end
 
 
-    assertEquals(Std.string(50),Std.string(scale.value(100)));
+    assertEquals(50,scale.value(100));
 
-    assertEquals(Std.string(99.49494949494951),Std.string(scale.value(200)));
+    assertEquals(99.49494949494951,scale.value(200));
 #if js
-    assertEquals(Std.string(200.00000000000003),Std.string(scale.invert(99.49494949494951)));
+    assertEquals(200.00000000000003,scale.invert(99.49494949494951));
 #else
+
+//    expected '200' but was '200' !!!!
     assertEquals(Std.string(200),Std.string(scale.invert(99.49494949494951)));
 #end
 
