a3a9aa2eb934b847ac0307b8c5f40b2268523961
tccoll <thomas.cervantes.collier@monsanto.com>
2015-12-29 20:20:05 -0600

    still writing initial tests

--- Source/MainTest.hx
+++ Source/MainTest.hx
@@ -3,7 +3,8 @@ class MainTest {
   static function main() {
     var r = new haxe.unit.TestRunner();
     r.add(new test.scale.Scale());
-    r.add(new test.utility.Bounds());
+    r.add(new test.scale.Color());
+//    r.add(new test.utility.Bounds());
 // add other TestCases here
 
 // finally, run the tests
